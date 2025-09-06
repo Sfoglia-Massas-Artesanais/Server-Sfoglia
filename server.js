3494e7732d44ec7c3279dea112cc631ccaf30bcd
unknown <fienotagliatelle@gmail.com>
2025-09-06 15:05:46 -0300

    Adicionando frontend no CORS

--- server.js
+++ server.js
@@ -6,14 +6,39 @@ import feedbackRoutes from "./src/routes/feedbackRoutes.js";
 import carrosselRoutes from "./src/routes/carrosselRoutes.js";
 
 const app = express();
-app.use(cors());
+
+// ======================
+// 🔹 Configuração do CORS
+// ======================
+const allowedOrigins = [
+  "http://localhost:5500",
+  "http://127.0.0.1:5500/",
+  "https://server-sfoglia.onrender.com",
+  "https://sfoglia-massas-artesanais.github.io/Sfogliafiennos/",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("🚫 Bloqueado por CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/feedback", feedbackRoutes);
 app.use("/carrossel", carrosselRoutes);
-
-// Rotas
 app.use("/auth", authRoutes);
 app.use("/pratos", pratoRoutes);
+app.get("/", (req, res) => {
+  res.send("🍝 Sfoglia Backend rodando com sucesso!");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
